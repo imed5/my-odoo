8ec6b5b259ef0b931c1166240644443cfe682580
imed5 <32360847+imed5@users.noreply.github.com>
2017-10-09 19:35:02 +0100

    Update __manifest__.py

--- control_credit_limit/__manifest__.py
+++ control_credit_limit/__manifest__.py
@@ -3,13 +3,12 @@
     'name': "Control the Credit Limit",
     'summary': "Allows a credit limit to be set for partners",
     'description': """
-        This plugin can be used to limit the allowable credit for a partner can have. 
-	All new credit transactions are checked against the credit limit and the accumulated owed credit to validate new sale
+	Approve Sale Orders Based on customers pre-set credit limit
     """,
     'author': "ERPish.com",
     'website': "http://www.erpish.com",
     'category': 'Partner',
-    'version': '10.0.9',   
+    'version': '10.0.11',   
     'depends': ['account','sale'],
     'data': [
         'views/partner_credit_view.xml',
