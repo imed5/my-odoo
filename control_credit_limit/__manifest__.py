6ee1bbcc2c228ffde01e84ab744fe487c848dd6e
Imed <imed@imed.ws>
2017-10-14 14:53:54 +0100

    adding another

--- control_credit_limit/__manifest__.py
+++ control_credit_limit/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Control the Credit Limit",
+    'name': "Control Sale Orders by Credit Limit",
     'summary': "Allows a credit limit to be set for partners",
     'description': """
 	Approve Sale Orders Based on customers pre-set credit limit
     """, 
     'author': "ERPish.com",
     'website': "http://www.erpish.com",
-    'category': 'Partner',
-    'version': '10.0.2.0',   
+    'category': 'Sales',
+    'version': '10.0.2.1',   
     'depends': ['account','sale'],
     'data': [
         'views/partner_credit_view.xml',
